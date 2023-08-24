5c1cce29f6d85fec00e328af2d263477b1284a02
Harkeerat Kanwal <hsk532991@gmail.com>
2023-08-24 12:03:41 -0400

    Put a box on the canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Canvas } from "@react-three/fiber";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div id="canvas-container">
       <Canvas>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshStandardMaterial />
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial />
+        </mesh>
 
         <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
+        <directionalLight color="grey" position={[0, 0, 5]} />
       </Canvas>
     </div>
   );
